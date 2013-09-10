7349d9fb105e99c4761e802c6ad427c42dc942b3
Jerad Bitner <sirkitree@gmail.com>
2013-09-09 20:01:35 -0600

    init

---
+++ app.js
@@ -0,0 +1,26 @@
+
+// Server to receive POST data.
+var http = require('http');
+ 
+http.createServer(function (req, res) {
+  // set up some routes
+  switch(req.url) {
+    case '/listener':
+      console.log(req.content);
+      break;
+  };
+}).listen(8080); // listen on tcp port 8080 (all interfaces)
+
+
+// var GitHubApi = require("github")
+//   , github = new GitHubApi({version: "3.0.0"})
+//   , auth_user = ''
+//   , auth_pass = ''
+//   , user = ''
+//   , repo = '';
+
+// github.authenticate({
+//     username: auth_user,
+//     password: auth_pass,
+//     type: "basic"
+// });
