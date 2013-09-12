cd1d1c79aede6e8a8939910a5f6fd5f26c7a4421
Jerad Bitner <sirkitree@gmail.com>
2013-09-11 21:27:12 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,42 @@
-// Server to receive POST data.
 var http = require('http');
- 
+var Querystring = require('querystring');
+
 http.createServer(function (req, res) {
-  // set up some routes
-  switch(req.url) {
-    case '/listener':
-      console.log(req.content);
-      break;
-  };
-}).listen(90); // listen on tcp port 8080 (all interfaces)
+  var buffer = [];
+  var bufferLength = 0;
+  var failed = false;
 
+  req.on('data', function (chunk) {
+    if (failed) return;
+    buffer.push(chunk);
+    bufferLength += chunk.length;
+  });
 
+  req.on('end', function(chunk) {
+    if (failed) return;
+    var data;
+
+    if (chunk) {
+      buffer.push(chunk);
+      bufferLength += chunk.length;
+    }
+
+    data = Buffer.concat(buffer, bufferLength).toString();
+    data = Querystring.parse(data).payload;
+    data = parse(data);
+    console.log(data);
+  });
+}).listen(3420);
+
+function parse(data) {
+  var result;
+  try {
+    result = JSON.parse(data);
+  } catch (e) {
+    result = false;
+  }
+  return result;
+}
 // var GitHubApi = require("github")
 //   , github = new GitHubApi({version: "3.0.0"})
 //   , auth_user = ''
